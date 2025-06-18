2aa96789b4a546e92e992f556fbc2224eba337b0
Nooom01 <Nooom01@users.noreply.github.com>
2025-06-18 14:35:40 -0400

    Clean up repository and update gitignore

--- test-env.js
+++
@@ -1,5 +0,0 @@
-// Temporary test file to check environment variables
-console.log('Testing environment variables:')
-console.log('NEXT_PUBLIC_SUPABASE_URL:', process.env.NEXT_PUBLIC_SUPABASE_URL)
-console.log('NEXT_PUBLIC_SUPABASE_ANON_KEY exists:', !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
-console.log('NEXT_PUBLIC_SUPABASE_ANON_KEY length:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.length)
